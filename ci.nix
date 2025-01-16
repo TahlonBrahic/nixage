7e5f64e6b3a936308cc943e35fa533ef88e193bc
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-15 22:01:18 -0600

    adjust outputs of CI

--- ci.nix
+++ ci.nix
@@ -1,23 +1,5 @@
 {pkgs ? import <nixpkgs> {}}:
 with builtins; let
-  isReserved = n: n == "lib" || n == "overlays" || n == "modules";
-  isDerivation = p: isAttrs p && p ? type && p.type == "derivation";
-  isBuildable = p: let
-    licenseFromMeta = p.meta.license or [];
-    licenseList =
-      if builtins.isList licenseFromMeta
-      then licenseFromMeta
-      else [licenseFromMeta];
-  in
-    !(p.meta.broken or false) && builtins.all (license: license.free or true) licenseList;
-  isCacheable = p: !(p.preferLocalBuild or false);
-  shouldRecurseForDerivations = p: isAttrs p && p.recurseForDerivations or false;
-
-  nameValuePair = n: v: {
-    name = n;
-    value = v;
-  };
-
   concatMap = builtins.concatMap or (f: xs: concatLists (map f xs));
 
   flattenPkgs = s: let
@@ -40,10 +22,7 @@ with builtins; let
       (map (n: nameValuePair n ageAttrs.${n})
         (filter (n: !isReserved n)
           (attrNames ageAttrs))));
-in rec {
-  buildPkgs = filter isBuildable agePkgs;
-  cachePkgs = filter isCacheable buildPkgs;
-
-  buildOutputs = concatMap outputsOf buildPkgs;
-  cacheOutputs = concatMap outputsOf cachePkgs;
+in {
+  buildOutputs = concatMap outputsOf agePkgs;
+  cacheOutputs = concatMap outputsOf agePkgs;
 }

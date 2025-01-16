166adaad6f23abe985ef1e644dacc1fb584a594d
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-15 20:24:16 -0600

    update pkg build instructions

--- default.nix
+++ default.nix
@@ -1,51 +1,48 @@
-{
-  lib,
-  buildGoModule,
-  fetchFromGitHub,
-  installShellFiles,
-}:
-buildGoModule rec {
-  pname = "nixage";
-  version = "0.0.1";
+{pkgs ? import <nixpkgs> {}}: let
+  inherit (pkgs) lib buildGoModule fetchFromGitHub installShellFiles;
+in
+  buildGoModule rec {
+    pname = "nixage";
+    version = "0.0.1";
 
-  src = fetchFromGitHub {
-    owner = "TahlonBrahic";
-    repo = "nixage";
-    rev = "v${version}";
-    hash = "";
-  };
+    src = fetchFromGitHub {
+      owner = "TahlonBrahic";
+      repo = "nixage";
+      rev = "v${version}";
+      hash = "";
+    };
 
-  vendorHash = "";
+    vendorHash = "";
 
-  ldflags = [
-    "-s"
-    "-w"
-    "-X main.Version=${version}"
-  ];
+    ldflags = [
+      "-s"
+      "-w"
+      "-X main.Version=${version}"
+    ];
 
-  nativeBuildInputs = [installShellFiles];
+    nativeBuildInputs = [installShellFiles];
 
-  doInstallCheck = true;
-  installCheckPhase = ''
-    if [[ "$("$out/bin/${pname}" --version)" == "${version}" ]]; then
-      echo '${pname} smoke check passed'
-    else
-      echo '${pname} smoke check failed'
-      return 1
-    fi
-  '';
+    doInstallCheck = true;
+    installCheckPhase = ''
+      if [[ "$("$out/bin/${pname}" --version)" == "${version}" ]]; then
+        echo '${pname} smoke check passed'
+      else
+        echo '${pname} smoke check failed'
+        return 1
+      fi
+    '';
 
-  # plugin test is flaky, see https://github.com/FiloSottile/age/issues/517
-  checkFlags = [
-    "-skip"
-    "TestScript/plugin"
-  ];
+    # plugin test is flaky, see https://github.com/FiloSottile/age/issues/517
+    checkFlags = [
+      "-skip"
+      "TestScript/plugin"
+    ];
 
-  meta = with lib; {
-    changelog = "https://github.com/TahlonBrahic/nixage/releases/tag/v${version}";
-    description = "Modern encryption tool with small explicit keys";
-    license = licenses.bsd3;
-    mainProgram = "nixage";
-    maintainers = [];
-  };
-}
+    meta = with lib; {
+      changelog = "https://github.com/TahlonBrahic/nixage/releases/tag/v${version}";
+      description = "Modern encryption tool with small explicit keys";
+      license = licenses.bsd3;
+      mainProgram = "nixage";
+      maintainers = [];
+    };
+  }
